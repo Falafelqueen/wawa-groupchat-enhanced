f78ee871cab51da4f134e6fa7f58f0802d598fe1
Lucia Harcegova <fakeluciamail@gmail.com>
2023-11-09 12:27:08 +0100

    Add custom color config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "rgb(34, 51, 122)",
+        dark: "rgb(17, 30, 84)",
+      },
+      secondary: "rgb(251, 248, 246)",
+      white: "white",
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
